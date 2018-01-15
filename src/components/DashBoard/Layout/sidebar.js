3ffc116c2e9ebdc468e417ba07a0d2ca7dbf27dd
Marcelo Menezes <marcelomenezes.0907@gmail.com>
2018-01-15 11:54:42 -0200

    Update

--- src/components/DashBoard/Layout/sidebar.js
+++ src/components/DashBoard/Layout/sidebar.js
@@ -1,11 +1,11 @@
 const sidebarLinks =  [
-  { icon: 'dashboard', text: 'Overview', path: '' },
+  { icon: 'dashboard', text: 'Overview', path: '/' },
   { icon: 'home', text: 'Imóveis', path: ''  },
-  { icon: 'person', text: 'Clientes', path: ''  },
+  { icon: 'person', text: 'Clientes', path: '/admin/clientes'  },
   { icon: 'assignment', text: 'Contratos', path: ''  },
   { icon: 'phonelink', text: 'App downloads', path: ''  },
-  { icon: 'settings', text: 'Configurações', path: '/config'  },
-  { icon: 'help', text: 'Help', path: ''  }
+  { icon: 'settings', text: 'Configurações', path: '/configuracoes'  },
+  { icon: 'help', text: 'Help', path: '/admin/suporte'  }
 ]
 
 
--- src/router/dashboard.js
+++ src/router/dashboard.js
@@ -2,12 +2,14 @@
 import Layout from '../components/DashBoard/Layout/Default.vue'
 
 // Childrens
+import Suporte from '../components/DashBoard/Views/Suporte.vue'
 import Overview from '../components/DashBoard/Views/Overview.vue'
 import Clientes from '../components/DashBoard/Views/Clientes.vue'
 
 const children = [
     { path: '', component: Overview },
-    { path: 'clientes', component: Clientes }
+    { path: 'clientes', component: Clientes },
+    { path: 'suporte', component: Suporte }
 ]
 
 const dashboard = { path: '/admin', component: Layout, meta: { requiresAuth: true }, children: children }
